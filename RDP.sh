065668f8cc88d7642a840d1d6f77ca75bc370e5c
OmarKhatib <elkhatibomar@outlook.com>
2021-01-08 09:50:45 +0200

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -18,7 +18,7 @@ sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano file-roller unrar vlc qbittorrent -y 
+sudo apt install nautilus nano file-roller unrar vlc qbittorrent vim gedit -y 
 sudo adduser lab chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
